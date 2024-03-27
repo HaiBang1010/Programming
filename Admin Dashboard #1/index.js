b9df44c6ab7f0f8e2e76c941c75fd0139a8223a6
HaiBang1010 <haibang1010999@gmail.com>
2024-03-27 14:11:49 +0700

    successfully

--- Admin Dashboard #1/index.js
+++ Admin Dashboard #1/index.js
@@ -16,3 +16,28 @@ Orders.forEach((order) => {
     tr.innerHTML = trContent;
     document.querySelector("table tbody").appendChild(tr);
 });
+
+const asideMenu = document.querySelector("aside");
+const btnMenu = document.querySelector(".menu");
+const btnClose = document.querySelector("#close");
+const btnDarkMode = document.querySelector(".dark-mode");
+
+btnMenu.addEventListener("click", () => {
+    asideMenu.style.display = "block";
+});
+
+btnClose.addEventListener("click", () => {
+    asideMenu.style.display = "none";
+});
+
+btnDarkMode.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode-variables");
+    btnDarkMode.querySelector("span:nth-child(1)").classList.toggle("active");
+    btnDarkMode.querySelector("span:nth-child(2)").classList.toggle("active");
+});
+
+window.addEventListener("resize", () => {
+    if (window.innerWidth >= 768) {
+        asideMenu.style.display = "block";
+    }
+});
